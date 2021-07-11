4ca5fb8d9bc34a0ec7ea477fb95b31b0ecc30330
wtzhu13 <wtzhu_13@163.com>
2021-07-11 22:21:53 +0800

    test of LSC

--- LSC/src/LSCMesh.m
+++ LSC/src/LSCMesh.m
@@ -21,6 +21,8 @@ title('org image');
 image_r = image(:,:,1);
 image_g = image(:,:,2);
 image_b = image(:,:,3);
+figure();
+imshow(image_r);
 
 % load gain of each channel
 load('./src/rGain.mat');
@@ -43,20 +45,56 @@ for i = 1:height
         end
         % get tht gain of the point by interpolation
         % 插值算法还有待改进
-        rGainP = rGain(gainStepX, gainStepY) * (j - (gainStepY-1) * sideY) / sideY +...
-                 rGain(gainStepX, gainStepY + 1) * (gainStepY * sideY - j) / sideY;
-        gGainP = gGain(gainStepX, gainStepY) * (j - (gainStepY-1) * sideY) / sideY +...
-                 gGain(gainStepX, gainStepY + 1) * (gainStepY * sideY - j) / sideY;
-        bGainP = bGain(gainStepX, gainStepY) * (j - (gainStepY-1) * sideY) / sideY +...
-                 bGain(gainStepX, gainStepY + 1) * (gainStepY * sideY - j) / sideY;
-        disImg(i, j, 1) =  image_r(i, j) * rGainP;
+%         if (i / sideX)-floor(i / sideX)<0.5
+            rGainP1 = rGain(gainStepX, gainStepY) * (gainStepY * sideY - j) / sideY   +...
+                     rGain(gainStepX+1, gainStepY + 1) *(j - (gainStepY-1) * sideY) / sideY;
+            rGainP2 = rGain(gainStepX, gainStepY+1) * (gainStepY * sideY - j) / sideY   +...
+                     rGain(gainStepX+1, gainStepY) *(j - (gainStepY-1) * sideY) / sideY;
+            rGainP = (rGainP1 + rGainP2)/2;
+            
+            gGainP1 = gGain(gainStepX, gainStepY) * (gainStepY * sideY - j) / sideY   +...
+                     gGain(gainStepX+1, gainStepY + 1) *(j - (gainStepY-1) * sideY) / sideY;
+            gGainP2 = gGain(gainStepX, gainStepY+1) * (gainStepY * sideY - j) / sideY   +...
+                     gGain(gainStepX+1, gainStepY) *(j - (gainStepY-1) * sideY) / sideY;
+            gGainP = (gGainP1 + gGainP2)/2;
+            
+            bGainP1 = bGain(gainStepX, gainStepY) * (gainStepY * sideY - j) / sideY   +...
+                     bGain(gainStepX+1, gainStepY + 1) *(j - (gainStepY-1) * sideY) / sideY;
+            bGainP2 = bGain(gainStepX, gainStepY+1) * (gainStepY * sideY - j) / sideY   +...
+                     bGain(gainStepX+1, gainStepY) *(j - (gainStepY-1) * sideY) / sideY;
+            bGainP = (bGainP1 + bGainP2)/2;
+            
+%         else
+%            rGainP = rGain(gainStepX+1, gainStepY) * (gainStepY * sideY - j) / sideY   +...
+%                      rGain(gainStepX+1, gainStepY + 1) *(j - (gainStepY-1) * sideY) / sideY;
+%             gGainP = gGain(gainStepX+1, gainStepY) * (gainStepY * sideY - j) / sideY   +...
+%                      gGain(gainStepX+1, gainStepY + 1) *(j - (gainStepY-1) * sideY) / sideY;
+%             bGainP = bGain(gainStepX+1, gainStepY) * (gainStepY * sideY - j) / sideY   +...
+%                      bGain(gainStepX+1, gainStepY + 1) *(j - (gainStepY-1) * sideY) / sideY; 
+%         end
+
+%         rGainP = (rGain(gainStepX, gainStepY+1)-rGain(gainStepX, gainStepY)) * (gainStepY * sideY - j) + ...
+%                  (rGain(gainStepX+1, gainStepY)-rGain(gainStepX, gainStepY)) * (gainStepX * sideX - i)  + ...
+%                  (rGain(gainStepX+1, gainStepY+1)+rGain(gainStepX, gainStepY)-rGain(gainStepX, gainStepY+1)-rGain(gainStepX+1, gainStepY)) * ...
+%                  (gainStepY * sideY - j) * (gainStepX * sideX - i) + rGain(gainStepX, gainStepY);
+%         gGainP = (gGain(gainStepX, gainStepY+1)-gGain(gainStepX, gainStepY)) * (gainStepY * sideY - j) + ...
+%                  (gGain(gainStepX+1, gainStepY)-gGain(gainStepX, gainStepY)) * (gainStepX * sideX - i) + ...
+%                  (gGain(gainStepX+1, gainStepY+1)+gGain(gainStepX, gainStepY)-gGain(gainStepX, gainStepY+1)-gGain(gainStepX+1, gainStepY)) * ...
+%                  (gainStepY * sideY - j) * (gainStepX * sideX - i) + gGain(gainStepX, gainStepY);
+%         bGainP = (bGain(gainStepX, gainStepY+1)-bGain(gainStepX, gainStepY)) * (gainStepY * sideY - j) + ...
+%                  (bGain(gainStepX+1, gainStepY)-bGain(gainStepX, gainStepY)) * (gainStepX * sideX - i) + ...
+%                  (bGain(gainStepX+1, gainStepY+1)+bGain(gainStepX, gainStepY)-bGain(gainStepX, gainStepY+1)-bGain(gainStepX+1, gainStepY)) * ...
+%                  (gainStepY * sideY - j) * (gainStepX * sideX - i) + bGain(gainStepX, gainStepY);
+        
+
+        disImg(i, j, 1) =  image_r(i, j) * rGainP;    
         disImg(i, j, 2) =  image_g(i, j) * gGainP;
         disImg(i, j, 3) =  image_b(i, j) * bGainP;
     end
 end
-disImg = uint8(disImg);
+
 figure();
-imshow(disImg);
+imshow(uint8(disImg));
 title('dis image')
         
         
