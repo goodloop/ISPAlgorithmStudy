98419c7c1f06842237f59d768016b29c1c89e578
wtzhu13 <wtzhu_13@163.com>
2022-01-24 16:20:13 +0800

    The Wn of red/blue to be interpolated to G is Ok

--- Demosaic/src/DW_Wn.m
+++ Demosaic/src/DW_Wn.m
@@ -9,7 +9,7 @@ function Wn = DW_Wn(neighborhoodData, directionNum, channelDeal, channelAdded)
 %   Instructions:
 %       author:     wtzhu
 %       e-mail:     wtzhu_13@163.com
-% Last Modified by wtzhu v1.0 2022-01-23
+% Last Modified by wtzhu v1.0 2022-01-24
 % Note: 
 if nargin < 3
     channelDeal = 1;
@@ -20,105 +20,70 @@ Wn = zeros(directionNum, 1);
 [h, w, c] = size(neighborhoodData);
 centerH = round(h/2);
 centerW = round(w/2);
-if c == 1
-    switch directionNum
-        case 12
-            In(1) = abs(neighborhoodData(centerH, centerW-1) - neighborhoodData(centerH, centerW+1)) + ...
-                    abs(neighborhoodData(centerH, centerW-2) - neighborhoodData(centerH, centerW));
 
-            In(2) = abs(neighborhoodData(centerH-1, centerW) - neighborhoodData(centerH+1, centerW)) + ...
-                    abs(neighborhoodData(centerH-2, centerW) - neighborhoodData(centerH, centerW));
+switch directionNum
+    case 12
+        In(1) = abs(neighborhoodData(centerH, centerW-1, channelAdded) - neighborhoodData(centerH, centerW+1, channelAdded)) + ...
+                abs(neighborhoodData(centerH, centerW-2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal));
 
-            In(3) = abs(neighborhoodData(centerH, centerW+1) - neighborhoodData(centerH, centerW-1)) + ...
-                    abs(neighborhoodData(centerH, centerW+2) - neighborhoodData(centerH, centerW));
+        In(2) = abs(neighborhoodData(centerH-1, centerW, channelAdded) - neighborhoodData(centerH+1, centerW, channelAdded)) + ...
+                abs(neighborhoodData(centerH-2, centerW, channelDeal) - neighborhoodData(centerH, centerW, channelDeal));
 
-            In(4) = abs(neighborhoodData(centerH+1, centerW) - neighborhoodData(centerH-1, centerW)) + ...
-                    abs(neighborhoodData(centerH+2, centerW) - neighborhoodData(centerH, centerW));    
+        In(3) = abs(neighborhoodData(centerH, centerW+1, channelAdded) - neighborhoodData(centerH, centerW-1, channelAdded)) + ...
+                abs(neighborhoodData(centerH, centerW+2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal));
 
-            In(5) = 0.5*(abs(neighborhoodData(centerH-1, centerW-2) - neighborhoodData(centerH+1, centerW+2)) + ...
-                         abs(neighborhoodData(centerH-2, centerW-4) - neighborhoodData(centerH, centerW)));
+        In(4) = abs(neighborhoodData(centerH+1, centerW, channelAdded) - neighborhoodData(centerH-1, centerW, channelAdded)) + ...
+                abs(neighborhoodData(centerH+2, centerW) - neighborhoodData(centerH, centerW));    
 
-            In(6) = 0.5*(abs(neighborhoodData(centerH-2, centerW-1) - neighborhoodData(centerH+2, centerW+1)) + ...
-                         abs(neighborhoodData(centerH-4, centerW-2) - neighborhoodData(centerH, centerW)));
+        In(5) = 0.5*(abs(neighborhoodData(centerH-1, centerW-2, channelAdded) - neighborhoodData(centerH+1, centerW+2, channelAdded)) + ...
+                     abs(neighborhoodData(centerH-2, centerW-4, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
 
-            In(7) = 0.5*(abs(neighborhoodData(centerH-2, centerW+1) - neighborhoodData(centerH+2, centerW-1)) + ...
-                         abs(neighborhoodData(centerH-4, centerW+2) - neighborhoodData(centerH, centerW)));
+        In(6) = 0.5*(abs(neighborhoodData(centerH-2, centerW-1, channelAdded) - neighborhoodData(centerH+2, centerW+1, channelAdded)) + ...
+                     abs(neighborhoodData(centerH-4, centerW-2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
 
-            In(8) = 0.5*(abs(neighborhoodData(centerH-1, centerW+2) - neighborhoodData(centerH+1, centerW-2)) + ...
-                         abs(neighborhoodData(centerH-2, centerW+4) - neighborhoodData(centerH, centerW)));
+        In(7) = 0.5*(abs(neighborhoodData(centerH-2, centerW+1, channelAdded) - neighborhoodData(centerH+2, centerW-1, channelAdded)) + ...
+                     abs(neighborhoodData(centerH-4, centerW+2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
 
-            In(9) = 0.5*(abs(neighborhoodData(centerH+1, centerW+2) - neighborhoodData(centerH-1, centerW-2)) + ...
-                         abs(neighborhoodData(centerH+2, centerW+4) - neighborhoodData(centerH, centerW))); 
+        In(8) = 0.5*(abs(neighborhoodData(centerH-1, centerW+2, channelAdded) - neighborhoodData(centerH+1, centerW-2, channelAdded)) + ...
+                     abs(neighborhoodData(centerH-2, centerW+4, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
 
-            In(10) = 0.5*(abs(neighborhoodData(centerH+2, centerW+1) - neighborhoodData(centerH-2, centerW-1)) + ...
-                          abs(neighborhoodData(centerH+4, centerW+2) - neighborhoodData(centerH, centerW)));
+        In(9) = 0.5*(abs(neighborhoodData(centerH+1, centerW+2, channelAdded) - neighborhoodData(centerH-1, centerW-2, channelAdded)) + ...
+                     abs(neighborhoodData(centerH+2, centerW+4, channelDeal) - neighborhoodData(centerH, centerW, channelDeal))); 
 
-            In(11) = 0.5*(abs(neighborhoodData(centerH+2, centerW-1) - neighborhoodData(centerH-2, centerW+1)) + ...
-                          abs(neighborhoodData(centerH+4, centerW-2) - neighborhoodData(centerH, centerW)));
+        In(10) = 0.5*(abs(neighborhoodData(centerH+2, centerW+1, channelAdded) - neighborhoodData(centerH-2, centerW-1, channelAdded)) + ...
+                      abs(neighborhoodData(centerH+4, centerW+2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
 
-            In(12) = 0.5*(abs(neighborhoodData(centerH+1, centerW-2) - neighborhoodData(centerH-1, centerW+2)) + ...
-                          abs(neighborhoodData(centerH+2, centerW-4) - neighborhoodData(centerH, centerW)));  
-            sumIn = 0;
-            for i =1 :12
-                sumIn = sumIn + (1/(1+In(i)));
-            end
-            for i =1 :12
-                Wn(i) = (1/(1+In(i)))/sumIn;
-            end
-        case 4
-            In(1) = abs(neighborhoodData(centerH-1, centerW-1) - neighborhoodData(centerH+1, centerW+1)) + ...
-                    abs(neighborhoodData(centerH-2, centerW-2) - neighborhoodData(centerH, centerW));
+        In(11) = 0.5*(abs(neighborhoodData(centerH+2, centerW-1, channelAdded) - neighborhoodData(centerH-2, centerW+1, channelAdded)) + ...
+                      abs(neighborhoodData(centerH+4, centerW-2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
 
-            In(2) = abs(neighborhoodData(centerH-1, centerW+1) - neighborhoodData(centerH+1, centerW-1)) + ...
-                    abs(neighborhoodData(centerH-2, centerW+2) - neighborhoodData(centerH, centerW));
+        In(12) = 0.5*(abs(neighborhoodData(centerH+1, centerW-2, channelAdded) - neighborhoodData(centerH-1, centerW+2, channelAdded)) + ...
+                      abs(neighborhoodData(centerH+2, centerW-4, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));   
+        sumIn = 0;
+        for i =1 :12
+            sumIn = sumIn + (1/(1+In(i)));
+        end
+        for i =1 :12
+            Wn(i) = (1/(1+In(i)))/sumIn;
+        end
+    case 4
+        In(1) = abs(neighborhoodData(centerH-1, centerW-1) - neighborhoodData(centerH+1, centerW+1)) + ...
+                abs(neighborhoodData(centerH-2, centerW-2) - neighborhoodData(centerH, centerW));
 
-            In(3) = abs(neighborhoodData(centerH+1, centerW+1) - neighborhoodData(centerH-1, centerW-1)) + ...
-                    abs(neighborhoodData(centerH+2, centerW+2) - neighborhoodData(centerH, centerW));
+        In(2) = abs(neighborhoodData(centerH-1, centerW+1) - neighborhoodData(centerH+1, centerW-1)) + ...
+                abs(neighborhoodData(centerH-2, centerW+2) - neighborhoodData(centerH, centerW));
 
-            In(4) = abs(neighborhoodData(centerH+1, centerW-1) - neighborhoodData(centerH-1, centerW+1)) + ...
-                    abs(neighborhoodData(centerH+2, centerW-2) - neighborhoodData(centerH, centerW));
-            sumIn = 0;
-            for i =1 :4
-                sumIn = sumIn + (1/(1+In(i)));
-            end
-            for i =1 :4
-                Wn(i) = (1/(1+In(i)))/sumIn;
-            end
-    end   
-else
-    In(1) = abs(neighborhoodData(centerH, centerW-1, channelAdded) - neighborhoodData(centerH, centerW+1, channelAdded)) + ...
-                    abs(neighborhoodData(centerH, centerW-2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal));
+        In(3) = abs(neighborhoodData(centerH+1, centerW+1) - neighborhoodData(centerH-1, centerW-1)) + ...
+                abs(neighborhoodData(centerH+2, centerW+2) - neighborhoodData(centerH, centerW));
 
-    In(2) = abs(neighborhoodData(centerH-1, centerW, channelAdded) - neighborhoodData(centerH+1, centerW, channelAdded)) + ...
-            abs(neighborhoodData(centerH-2, centerW, channelDeal) - neighborhoodData(centerH, centerW, channelDeal));
+        In(4) = abs(neighborhoodData(centerH+1, centerW-1) - neighborhoodData(centerH-1, centerW+1)) + ...
+                abs(neighborhoodData(centerH+2, centerW-2) - neighborhoodData(centerH, centerW));
+        sumIn = 0;
+        for i =1 :4
+            sumIn = sumIn + (1/(1+In(i)));
+        end
+        for i =1 :4
+            Wn(i) = (1/(1+In(i)))/sumIn;
+        end
+end   
 
-    In(3) = abs(neighborhoodData(centerH, centerW+1, channelAdded) - neighborhoodData(centerH, centerW-1, channelAdded)) + ...
-            abs(neighborhoodData(centerH, centerW+2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal));
-
-    In(4) = abs(neighborhoodData(centerH+1, centerW, channelAdded) - neighborhoodData(centerH-1, centerW, channelAdded)) + ...
-            abs(neighborhoodData(centerH+2, centerW) - neighborhoodData(centerH, centerW));    
-
-    In(5) = 0.5*(abs(neighborhoodData(centerH-1, centerW-2, channelAdded) - neighborhoodData(centerH+1, centerW+2, channelAdded)) + ...
-                 abs(neighborhoodData(centerH-2, centerW-4, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
-
-    In(6) = 0.5*(abs(neighborhoodData(centerH-2, centerW-1, channelAdded) - neighborhoodData(centerH+2, centerW+1, channelAdded)) + ...
-                 abs(neighborhoodData(centerH-4, centerW-2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
-
-    In(7) = 0.5*(abs(neighborhoodData(centerH-2, centerW+1, channelAdded) - neighborhoodData(centerH+2, centerW-1, channelAdded)) + ...
-                 abs(neighborhoodData(centerH-4, centerW+2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
-
-    In(8) = 0.5*(abs(neighborhoodData(centerH-1, centerW+2, channelAdded) - neighborhoodData(centerH+1, centerW-2, channelAdded)) + ...
-                 abs(neighborhoodData(centerH-2, centerW+4, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
-
-    In(9) = 0.5*(abs(neighborhoodData(centerH+1, centerW+2, channelAdded) - neighborhoodData(centerH-1, centerW-2, channelAdded)) + ...
-                 abs(neighborhoodData(centerH+2, centerW+4, channelDeal) - neighborhoodData(centerH, centerW, channelDeal))); 
-
-    In(10) = 0.5*(abs(neighborhoodData(centerH+2, centerW+1, channelAdded) - neighborhoodData(centerH-2, centerW-1, channelAdded)) + ...
-                  abs(neighborhoodData(centerH+4, centerW+2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
-
-    In(11) = 0.5*(abs(neighborhoodData(centerH+2, centerW-1, channelAdded) - neighborhoodData(centerH-2, centerW+1, channelAdded)) + ...
-                  abs(neighborhoodData(centerH+4, centerW-2, channelDeal) - neighborhoodData(centerH, centerW, channelDeal)));
-
-    In(12) = 0.5*(abs(neighborhoodData(centerH+1, centerW-2, channelAdded) - neighborhoodData(centerH-1, centerW+2, channelAdded)) + ...
-                  abs(neighborhoodData(centerH+2, centerW-4, channelDeal) - neighborhoodData(centerH, centerW, channelDeal))); 
-end
+    
